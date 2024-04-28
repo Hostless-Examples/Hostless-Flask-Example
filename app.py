72aeaaaaa069b381d17b86b74e9686b8c97e7176
Adesoji Daniel <sojidaniels@gmail.com>
2024-04-28 11:10:12 +0100

    initial commit

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=os.environ.get('PORT', 8000), debug=True)
